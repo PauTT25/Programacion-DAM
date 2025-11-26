2ed75c3b881b22d6c79208af24fb1690bcd0744b
PauTT25 <contrerasromeropau@gmail.com>
2025-11-26 09:51:07 +0100

    1

---
+++ Programacion/006 aplicacion de las estructuras de almacenamiento/006 colecciones listas, conjuntos y diccionarios/002 diccionrios ahora si.py
@@ -0,0 +1,15 @@
+persona = [
+	"Pau",
+	"Contreras Romero",
+	"pau@gmail.com",
+	20
+]
+
+#Ahora en formato diccionario
+
+persona = {
+	"nombre":"Pau",
+	"apellidos":"Contreras Romero",
+	"correo":"pau@gmail.com",
+	"edad":20
+}
---
+++ Programacion/006 aplicacion de las estructuras de almacenamiento/006 colecciones listas, conjuntos y diccionarios/003 lista en diccionario.py
@@ -0,0 +1,10 @@
+persona = {
+	"nombre":"Pau",
+	"apellidos":"Contreras Romero",
+	"correo":"pau@gmail.com",
+	"edad":20,
+	"telefonos":[
+		123456789,
+		987654321
+   	]
+}

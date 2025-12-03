8065b95f59ca2b640b0c7347cfdacada4744c292
PauTT25 <contrerasromeropau@gmail.com>
2025-12-03 13:10:08 +0000

    1

---
+++ Bases de datos/U4 Tratamiento de datos/001 Insercion, borrado y modificacion de registros/004 insercion parte 1.sql
@@ -0,0 +1,11 @@
+INSERT INTO equipos VALUES(
+	NULL,
+	"Levante",
+	"Valencia",
+	"Ciutat de Valencia",
+	1919,
+	10000000.00,
+  "https://levantecf.es"
+);
+
+SELECT * FROM equipos;
---
+++ Bases de datos/U4 Tratamiento de datos/001 Insercion, borrado y modificacion de registros/009 crear como tabla.php
@@ -0,0 +1,15 @@
+<?php
+  $host = "localhost";$user = "futbol2526";
+  $pass = "Futbol2526$";$db   = "futbol2526";
+  $conexion = new mysqli($host, $user, $pass, $db);
+  $sql = "SELECT * FROM equipos";
+  $resultado = $conexion->query($sql);
+  while ($fila = $resultado->fetch_assoc()) {
+    foreach($fila as $clave=>$valor){
+    	
+    }
+  }
+
+  $conexion->close();
+  
+?>

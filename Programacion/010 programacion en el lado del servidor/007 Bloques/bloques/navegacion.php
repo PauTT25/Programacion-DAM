70f1796264233e0152d40ba3afb4ed213add801d
PauTT25 <contrerasromeropau@gmail.com>
2025-12-05 09:26:27 +0000

    05/12/2025

---
+++ Programacion/010 programacion en el lado del servidor/007 Bloques/bloques/navegacion.php
@@ -0,0 +1,8 @@
+<ul>
+    		<li><a href="enlance.php">Enlace</a></li>
+    		<li><a href="enlance.php">Enlace</a></li>
+    		<li><a href="enlance.php">Enlace</a></li>
+    		<li><a href="enlance.php">Enlace</a></li>
+    		<li><a href="enlance.php">Enlace</a></li>
+    		<li><a href="enlance.php">Enlace</a></li>
+			</ul>

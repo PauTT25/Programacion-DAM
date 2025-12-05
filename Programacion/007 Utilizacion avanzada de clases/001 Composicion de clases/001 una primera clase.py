897919da671f88dd42d656030fa0b3355a15a08c
PauTT25 <contrerasromeropau@gmail.com>
2025-12-05 08:15:26 +0000

    05/12/2025

---
+++ Programacion/007 Utilizacion avanzada de clases/001 Composicion de clases/001 una primera clase.py
@@ -0,0 +1,7 @@
+class Alumno():
+	def __init__ (self,nombre,apellidos,email):
+		self.nombre = nombre
+		self.apellidos = apellidos
+		self.email = email
+alumno1 = Alumno("Pau","Contreras","pau@gmail.com")
+print(alumno1)
---
+++ Programacion/007 Utilizacion avanzada de clases/001 Composicion de clases/005 subclases.py
@@ -0,0 +1,32 @@
+class Persona():
+  def __init__(self,nombre,apellidos,email,direccion):
+    self.nombre = nombre
+    self.apellidos = apellidos
+    self.email = email
+    self.direccion = direccion
+  def dameDatos(self):
+    return self.nombre+self.apellidos
+
+class Profesor(Persona):
+  def __init__(self,nombre,apellidos,email,direccion):
+  	super().__init__(nombre, apellidos, email,direccion)			#Cuando utilizemos super en python tenemos que indicarle que queremos que nos traiga en la clase, si nos nos 
+  
+class Alumno(Persona):
+  def __init__(self,nombre,apellidos,email,direccion):
+    super().__init__(nombre, apellidos, email,direccion)
+    
+class AlumnoOnline(Alumno):
+	def __init__(self,nombre,apellidos,email,direccion):
+    super().__init__(nombre, apellidos, email,direccion)
+    
+class AlumnoPresencial(Alumno):
+	def __init__(self,nombre,apellidos,email,direccion):
+    super().__init__(nombre, apellidos, email,direccion)
+	
+  
+    
+alumno1 = Alumno("Pau","Contreras","pau@gmail.com","calle de Pau")
+print(alumno1.dameDatos())
+
+profesor1 = Profesor("Juan","Garcia","juan@jocarsa.com","calle de Juan")
+print(profesor1.dameDatos())

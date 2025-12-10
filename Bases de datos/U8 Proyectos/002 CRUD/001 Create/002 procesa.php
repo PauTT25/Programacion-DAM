ef99b88d4cc7ce5ad9604a5d1c5cda225fb42fac
PauTT25 <contrerasromeropau@gmail.com>
2025-12-10 13:14:39 +0000

    1

---
+++ Bases de datos/U8 Proyectos/002 CRUD/001 Create/002 procesa.php
@@ -0,0 +1,34 @@
+<?php
+	// Primero cogemos la info que viene del formulario
+  $nombre = $_POST['nombre'];
+  $salario = $_POST['puesto'];
+  $nombre = $_POST['salario'];
+  $fecha_contratacion = $_POST['fecha_contratacion'];
+  $departamento = $_POST['departamento']; 
+ 
+	// Y luego metemos esa información en la base de datos
+  $host = "localhost";
+  $user = "empleados";
+  $pass = "Empleados123$";
+  $db   = "superaplicacion";
+
+  $conexion = new mysqli($host, $user, $pass, $db);
+  
+	// Metemos los datos en la base de datos
+  $sql = "
+  			INSERT INTO empleados VALUES(
+    		NULL,
+      '".$nombre."',
+      '".$puesto."',
+      '".$salario."',
+      '".$fecha_contratacion."',
+      '".$departamento."'
+    );
+  ";
+	
+	$conexion->query($sql);  
+	
+  $conexion->close();
+  
+
+?>

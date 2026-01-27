35127137eb89f842745e03eff5d27251db442642
PauTT25 <contrerasromeropau@gmail.com>
2026-01-27 11:06:55 +0000

    1

---
+++ Programacion/010 programacion en el lado del servidor/013 Idiomas en programacion/003-array interno.php
@@ -0,0 +1,31 @@
+<?php
+	$idioma['es']['inicio'] = "Inicio";
+	$idioma['es']['sobremi'] = "Sobre mi";
+	$idioma['es']['proyectos'] = "Proyectos";
+	$idioma['es']['contactos'] = "COntactos";
+	
+	$idioma['en']['inicio'] = "Home";
+  $idioma['en']['sobremi'] = "About me";
+  $idioma['en']['proyectos'] = "Projects";
+  $idioma['en']['contacto'] = "Contact";
+?>
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Multi idioma</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <select>
+      <option value="es">🇪🇸</option>
+      <option value="en">🇬🇧</option>
+    </select>
+    <h1>Pau Contreras Romero</h1>
+    <nav>
+    	<a href="">Inicio</a>
+    	<a href="">Sobre mi</a>
+    	<a href="">Proyectos</a>
+    	<a href="">Contactos</a>
+  </body>
+</html>
---
+++ Programacion/010 programacion en el lado del servidor/013 Idiomas en programacion/004-por defecto en español.php
@@ -0,0 +1,31 @@
+<?php
+	$idioma['es']['inicio'] = "Inicio";
+	$idioma['es']['sobremi'] = "Sobre mi";
+	$idioma['es']['proyectos'] = "Proyectos";
+	$idioma['es']['contactos'] = "COntactos";
+	
+	$idioma['en']['inicio'] = "Home";
+  $idioma['en']['sobremi'] = "About me";
+  $idioma['en']['proyectos'] = "Projects";
+  $idioma['en']['contacto'] = "Contact";
+?>
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Multi idioma</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <select>
+      <option value="es">🇪🇸</option>
+      <option value="en">🇬🇧</option>
+    </select>
+    <h1>Pau Contreras Romero</h1>
+    <nav>
+    	<a href=""><?= $idioma['es']['inicio'] ?></a>
+      <a href=""><?= $idioma['es']['sobremi'] ?></a>
+      <a href=""><?= $idioma['es']['proyectos'] ?></a>
+      <a href=""><?= $idioma['es']['contacto'] ?></a>
+  </body>
+</html>

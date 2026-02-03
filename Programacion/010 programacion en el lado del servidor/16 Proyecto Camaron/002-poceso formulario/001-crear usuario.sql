d5f14f5b1adbefcbbe1a3e520c1c80bbabd59122
PauTT25 <contrerasromeropau@gmail.com>
2026-02-03 11:30:44 +0000

    1

---
+++ Programacion/010 programacion en el lado del servidor/16 Proyecto Camaron/002-poceso formulario/001-crear usuario.sql
@@ -0,0 +1,26 @@
+-- crea usuario nuevo con contraseña
+-- creamos el nombre de usuario que queramos
+CREATE USER 
+'camaron'@'localhost' 
+IDENTIFIED  BY 'Camaron123$';
+
+-- permite acceso a ese usuario
+GRANT USAGE ON *.* TO 'camaron'@'localhost';
+--[tuservidor] == localhost
+-- La contraseña puede requerir Mayus, minus, numeros, caracteres, min len
+
+-- quitale todos los limites que tenga
+ALTER USER 'camaron'@'localhost' 
+REQUIRE NONE 
+WITH MAX_QUERIES_PER_HOUR 0 
+MAX_CONNECTIONS_PER_HOUR 0 
+MAX_UPDATES_PER_HOUR 0 
+MAX_USER_CONNECTIONS 0;
+
+-- dale acceso a la base de datos empresadam
+GRANT ALL PRIVILEGES ON camaron.* 
+TO 'camaron'@'localhost';
+
+-- recarga la tabla de privilegios
+FLUSH PRIVILEGES;
+
